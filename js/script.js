61b0946dfb1d0ff6e72841e6ecc5b8c1aa1e1649
AlbericoD <a.diasbarreto@icloud.com>
2017-06-04 22:27:27 -0300

    Adicionado arquivos
    Upload de arquivos para o professor fazer download

---
+++ js/script.js
@@ -0,0 +1,125 @@
+/*ALERTA COM NOME E RA  NÃO FUNCIONA COMO ESPERADO ATUALIZAÇÃO DE SEGURANÇA DOS NAVEGADORES*/
+window.onbeforeunload = function(){
+  alert('None: Albérico Dias Barreto Filho RA:20171571\nGabriel Adriano Binoto RA:20160961');
+  return '';
+};
+
+/*LIVROS ACADEMICOS*/
+
+function livroAca1() {
+
+  window.confirm('Livro:   "Introdução à Programação"\nValor:   R$ 115,20\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+ 
+}
+
+function livroAca2() {
+
+  window.confirm('Livro:   "Introdução à Programação com Python"\nValor:   R$ 95,50\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+}
+
+function livroAca3() {
+
+  window.confirm('Livro:   "Use A Cabeça! Programação"\nValor:   R$ 77,41\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+}
+
+function livroAca4() {
+
+  window.confirm('Livro:   "Use A Cabeça! C"\nValor:   R$ 87,25\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+}
+
+function livroAca5() {
+
+  window.confirm('Livro:   "Use A Cabeça! Java"\nValor:   R$ 101,49\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+}
+
+function livroAca6() {
+
+  window.confirm('Livro:   "Use a Cabeça! SQL"\nValor:   R$ 83,99\nDisponivel em estoque :  ' + Math.floor((Math.random() * 10) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 10
+}
+
+/*LIVROS INFANTIS*/
+
+function livroInf1() {
+
+  window.confirm('Livro:   "Primeiro Livro Para Aprender a… Escrever"\nValor:   R$ 3,10\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+function livroInf2() {
+
+  window.confirm('Livro:   "Primeiro Livro Para Aprender a… Contar"\nValor:   R$ 4,52\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+function livroInf3() {
+
+  window.confirm('Livro:   "Meus Novos Labirintos - Coleção Labirintos Divertidos"\nValor:   R$ 2,72\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+function livroInf4() {
+
+  window.confirm('Livro:   "Meu Livro de Exercícios de… Calcular"\nValor:   R$ 6,78\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+function livroInf5() {
+
+  window.confirm('Livro:   "Livro Gigante de Atividades. 250 Atividades"\nValor:   R$ 14,90\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+function livroInf6() {
+
+  window.confirm('Livro:   "365 Atividades Divertidas"\nValor:   R$ 11,30\nDisponivel em estoque :  ' + Math.floor((Math.random() * 20) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 20
+}
+
+/*LIVROS Aventuras*/
+function livroAven1() {
+
+  window.confirm('Livro:   "Hora de Aventura. Edição Matemática"\nValor:   R$ 22,50\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+
+function livroAven2() {
+
+  window.confirm('Livro:   "Agente da Shield. Tiro Perfeito "\nValor:   R$ 27,42\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+function livroAven3() {
+
+  window.confirm('Livro:   "Aventuras  "\nValor:   R$ 23,90\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+
+function livroAven4() {
+
+  window.confirm('Livro:   "As Aventuras de Ook e Gluk   "\nValor:   R$ 14,90\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+function livroAven5() {
+
+  window.confirm('Livro:   "As Aventuras De Pi  "\nValor:   R$ 16,90\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+function livroAven6() {
+
+  window.confirm('Livro:   "As Crônicas de Nárnia. Brochura  "\nValor:   R$ 23,20\nDisponivel em estoque :  ' + Math.floor((Math.random() * 30) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 30
+}
+
+/*LIVROS PfC*/
+
+function livroPfC1() {
+
+  window.confirm('Livro:   "Caminhos para Roma: Aventura, queda e vitória do espírito "\nValor:   R$ 17,50\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
+function livroPfC2() {
+
+  window.confirm('Livro:   "Reflexões Sobre a Revolução na França "\nValor:   R$ 52,50\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
+function livroPfC3() {
+
+  window.confirm('Livro:   "A Rebelião das Massas "\nValor:   R$ 44,10\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
+function livroPfC4() {
+
+  window.confirm('Livro:   "A Mentalidade Anticapitalista"\nValor:   R$ 27,14\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
+function livroPfC5() {
+
+  window.confirm('Livro:   "O Livre Mercado e Seus Inimigos"\nValor:   R$ 25,90\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
+function livroPfC6() {
+
+  window.confirm('Livro:   "As Seis Lições"\nValor:   R$ 24,82\nDisponivel em estoque :  ' + Math.floor((Math.random() * 40) + 1) + '   exemplares ');//retorna a quantidade em estoque aleatória entre 1 e 40
+}
